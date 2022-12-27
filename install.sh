c5aeb76b0e8d6230bde39b7374586479a6dd3e04
Gabriel Martini <gabrielb.martini@gmail.com>
2022-12-27 09:44:32 -0300

    feat: updated packages

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # install brew
-/bin/bash -c $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+/bin/bash -c $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh) >/dev/null
+
+echo -ne '>>>                       [20%]\r'
 
 # install packages & cask with brew
 xargs brew install <brew/packages.txt
 
-# setup zsh & oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# TODO: PowerLevel, NerdFont, zshrc
+echo -ne '>>>>>>>                   [40%]\r'
 
 # install python versions
 asdf plugin-add python
@@ -19,10 +17,24 @@ asdf global python 3.8.8
 
 # install python global packages
 pip install -r python/global-requirements.txt
+asdf reshim python
+
+echo -ne '>>>>>>>>>>>>>>            [60%]\r'
+
+# setup zsh & oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp zshell/.zshrc ~/.zshrc
+
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
 
 # setup vscode
 cat vscode/extensions.txt | grep -v '^#' | xargs -L1 code --install-extension
 cp vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
+echo -ne '\n'
+
 # restart zsh
-source ~/.zshrc
+exec zsh
--- zshell/.zshrc
+++ zshell/.zshrc
@@ -1,5 +1,6 @@
+# ZSH
 ZSH_DISABLE_COMPFIX="true"
-export ZSH="/Users/martinig/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 plugins=(
     git
@@ -17,25 +18,31 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
-. $(brew --prefix asdf)/libexec/asdf.sh
-
 POWERLEVEL9K_MODE='nerdfont-complete'
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(user dir vcs newline status)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(os_icon load newline ssh asdf virtualenv)
 POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\ue0bc'
 POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR='\ue0b6'
 
+source $(brew --prefix powerlevel10k)/powerlevel10k.zsh-theme
+
+export PATH="/usr/local/sbin:$PATH"
+
+# ASDF
+. $(brew --prefix asdf)/libexec/asdf.sh
+
 alias virtualenv='python -m virtualenv'
 
-source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme
 . ~/.asdf/plugins/java/set-java-home.zsh
-export PATH="/usr/local/sbin:$PATH"
+
+export JAVA_TOOLS_OPTIONS="-Dlog4j2.formatMsgNoLookups=true"
+
+# SSL
+export OPENSSL_ROOT_DIR=$(brew --prefix openssl@3)
 
 # tabtab source for packages
 # uninstall by removing these lines
 [[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh || true
-export JAVA_TOOLS_OPTIONS="-Dlog4j2.formatMsgNoLookups=true"
-export OPENSSL_ROOT_DIR=/usr/local/opt/openssl@3
 
 autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C /usr/local/bin/terraform terraform
